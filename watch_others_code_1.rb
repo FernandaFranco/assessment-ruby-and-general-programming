79a3e5496f47c06708969aa40e39199a1d3dcdb2
Fernanda Franco <francodias.fernanda@gmail.com>
2016-10-03 18:28:29 -0400

    First exercises from first 'watch others code' recording

---
+++ watch_others_code_1.rb
@@ -0,0 +1,44 @@
+# def is_prime?(integer)
+#   return false if integer < 2
+#   (2...integer).each do |number|
+#     return false if integer % number == 0
+#   end
+#   true
+# end
+
+# p is_prime?(24)
+# p is_prime?(3)
+
+# def select_primes(array)
+#   array.select do |number|
+#     is_prime?(number)
+#   end
+# end
+
+# p select_primes([1,2,3,4])
+# p select_primes([4,6,8,10])
+
+# def count_primes(array)
+#   select_primes(array).count
+# end
+
+#   p count_primes([1,2,3,4])
+#   p count_primes([4,6,8,10])
+
+def prompt(message)
+  puts ">> #{message}"
+end
+
+prompt "Please enter an integer greater than 0:"
+integer = gets.chomp.to_i
+
+prompt "Enter 's' to compute the sum, 'p' to computer the product:"
+choice = gets.chomp.downcase
+
+if choice == 's'
+  sum = (1..integer).reduce(:+)
+  prompt "The sum of the integers between 1 and #{integer} is #{sum}"
+else
+  product = (1..integer).reduce(:*)
+  prompt "The product of the integers between 1 and #{integer} is #{product}"
+end
