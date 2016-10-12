144318078bcc0d9664da64531004124817f33a3b
Fernanda Franco <francodias.fernanda@gmail.com>
2016-10-12 16:32:24 -0400

    first and second woc 3 exxercises

--- watch_others_code_3.rb
+++ watch_others_code_3.rb
@@ -8,21 +8,41 @@ TRANSLATION = {
   "six" => 6,
   "seven" => 7,
   "eight" => 8,
-  "nine" => 9
+  "nine" => 9,
+  "ten" => 10
 }
 
+# def computer(english)
+#   words = english.split
+#   numbers_as_strings = words.select.with_index do |_, index|
+#     index.even?
+#   end
+#   numbers = numbers_as_strings.map do |number|
+#     TRANSLATION[number]
+#   end
+#   operations = words.select.with_index do |_, index|
+#     index.odd?
+#   end
+#   new_array = numbers
+#   operations.each do |operation|
+#     result = case operation
+#              when "plus"
+#                numbers[0] + numbers[1]
+#              when "minus"
+#                numbers[0] - numbers[1]
+#              end
+#     new_array.shift(2)
+#     new_array.unshift(result)
+#   end
+#   new_array[0]
+# end
+
 def computer(english)
-  numbers = [english.split[0], english.split[2]].map do |number|
-    TRANSLATION[number]
-  end
-  case english.split[1]
-  when "plus"
-    numbers.reduce(:+)
-  when "minus"
-    numbers.reduce(:-)
-  end
 end
 
 p computer("two plus two")
 p computer("seven minus six")
 p computer("zero plus eight")
+
+p computer("two plus two minus three")
+p computer("three minus one plus five minus four plus six plus ten minus four")
